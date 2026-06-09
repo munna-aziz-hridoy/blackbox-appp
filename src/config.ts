a14f44ec8437c1cd616f4fbf4d3915879e84ebc5
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2026-06-09 17:15:39 +0600

    change url

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const SERVER_URL = "https://blackbox-server-d557.onrender.com"
+// export const SERVER_URL = "https://blackbox-server-d557.onrender.com"
+export const SERVER_URL = "http://47.131.250.110"
